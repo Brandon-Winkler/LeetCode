1cf7b6baa27b68e90c8d8471bf8ca876a0761492
Brandon <bwinkler4@gmail.com>
2023-11-06 23:08:43 -0800

    file for number of subarrays in sliding window

---
+++ Sliding_Window/Medium/NumberOfSubarrays.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        
+    }
+};
+
+// driver program
+int main(){
+    return 0;
+}
