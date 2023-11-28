0956a3a298c7d82a8975017140616b9de436c0d5
Brandon <bwinkler4@gmail.com>
2023-11-27 23:59:02 -0800

    driver code for number of subarrays in sliding window

--- Sliding_Window/Medium/NumberOfSubarrays.cpp
+++ Sliding_Window/Medium/NumberOfSubarrays.cpp
@@ -11,5 +11,15 @@ public:
 
 // driver program
 int main(){
+    // initialize vectors
+    vector<int> v1 = {2,2,2,2,5,5,5,8};
+    vector<int> v2 = {11,13,17,23,29,31,7,5,2,3};
+
+    // solution class instance
+    Solution Solution;
+
+    // output
+    cout<<"The number subarrays with size 3 and a threshold of 4 for vector 1 are: "<<Solution.numOfSubarrays(v1,3,4)<<'\n';
+    cout<<"The number subarrays with size 3 and a threshold of 5 for vector 2 are: "<<Solution.numOfSubarrays(v2,3,5)<<'\n';
     return 0;
 }
