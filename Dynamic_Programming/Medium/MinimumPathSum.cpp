eecf0ad3827a40b74dee1653e1d93531c8eeb1a4
Brandon <bwinkler4@gmail.com>
2023-11-29 23:53:17 -0800

    commented solution to minimum path sum

--- Dynamic_Programming/Medium/MinimumPathSum.cpp
+++ Dynamic_Programming/Medium/MinimumPathSum.cpp
@@ -7,32 +7,32 @@ using namespace std;
 class Solution {
 public:
     int solve(int i, int j, vector<vector<int>>& grid, int n, int m, map<pair<int,int>,int>& memo){
-        if(i == n-1 && j == m-1) return grid[i][j];
+        if(i == n-1 && j == m-1) return grid[i][j];             // base case: if we are at the bottom right corner return the value at that corner
 
-        if(memo[{i,j}]) return memo[{i,j}];
+        if(memo[{i,j}]) return memo[{i,j}];                     // if the value already exists in memo then return that value
 
-        int ans = 0;
+        int ans = 0;                                            // intialize answer to be 0
 
-        if(i+1 < n && j+1 < m){  
+        if(i+1 < n && j+1 < m){                                 // if we are within the bounds of the grid then set the answer equal to the minimum of the next cell right or next cell down  
             ans = min(
                 grid[i][j]+solve(i+1, j, grid, n, m, memo),
                 grid[i][j]+solve(i, j+1, grid, n, m, memo)
             );
         }
-        else if(i+1 < n){
+        else if(i+1 < n){                                       // if the cell down is within bounds of row size then add the current sum to the cell value down
             ans = grid[i][j]+solve(i+1, j, grid, n, m, memo);
         }
-        else if(j+1 < m){
+        else if(j+1 < m){                                       // if the cell to the right is within the bounds of the column size then add the current sum to the cell value to the right 
             ans = grid[i][j]+solve(i, j+1, grid, n, m, memo);
         }
 
-        memo[{i,j}] = ans;
+        memo[{i,j}] = ans;                                      // store current answer value in memory
         return ans;
     }
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        map<pair<int,int>,int> memo;
+        int n = grid.size();                    // variable for size of rows
+        int m = grid[0].size();                 // variable for size of columns
+        map<pair<int,int>,int> memo;            // map for memory
         return solve(0, 0, grid, n, m, memo);
     }
 };
