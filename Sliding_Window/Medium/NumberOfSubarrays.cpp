a29dbde5a7e2961100ba9fda856f69fb73d21031
Brandon <bwinkler4@gmail.com>
2023-12-01 22:46:56 -0800

    solution for Number of subarrays

--- Sliding_Window/Medium/NumberOfSubarrays.cpp
+++ Sliding_Window/Medium/NumberOfSubarrays.cpp
@@ -5,7 +5,18 @@ using namespace std;
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
-        
+        int sum = 0, ans = 0;               // initialize sum to 0 to keep track of current window sum and answer to 0 to count number of windows
+        int l = 0, r = k;                   // initialize left and right side of window
+
+        for(int i=0;i<k;++i) sum += arr[i]; // add up all the numbers in the current window
+        if(sum/k >= threshold) ++ans;       // if the average of the numbers in the current window is less than or equal to the threshold add one to output
+
+        while(r < arr.size()){              // loop through array until right side is out of bounds
+            sum -= arr[l++];                // subtract the next left value from sum and increment left side
+            sum += arr[r++];                // add the next left value to sum and increment right side
+            if(sum/k >= threshold) ++ans;   // if the current sum is greater than or equal to the threshold then add one to the count
+        }
+        return ans;
     }
 };
 
