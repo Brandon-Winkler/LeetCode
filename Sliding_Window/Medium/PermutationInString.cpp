15054f641896ab2924cddedf0947e2362fe87956
Brandon <bwinkler4@gmail.com>
2023-12-09 22:28:51 -0800

    solution to permutation in string

--- Sliding_Window/Medium/PermutationInString.cpp
+++ Sliding_Window/Medium/PermutationInString.cpp
@@ -1,11 +1,34 @@
 #include<string>
 #include<iostream>
+#include<string>
+#include<vector>
 using namespace std;
 
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        
+    bool isPermutation(vector<int>& count){
+        for(int i=0;i<26;i++){
+            if(count[i] != 0) return false;
+        }
+        return true;
+    }
+    bool checkInclusion(string s1, string s2){
+        if(s1.size() > s2.size()) return false;
+
+        vector<int> count(26);
+        for(int i=0;i<s1.size();i++){
+            count[s1[i]-'a']++;
+            count[s2[i]-'a']--;
+        }
+
+        if(isPermutation(count)) return true;
+
+        for(int i=s1.size();i<s2.size();i++){
+            count[s2[i]-'a']--;
+            count[s2[i-s1.size()]-'a']++;
+            if(isPermutation(count)) return true;
+        }
+        return false;
     }
 };
 
