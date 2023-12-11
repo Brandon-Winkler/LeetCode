48812c558e2b3feabec71b55d6b9500e371bc28c
Brandon <bwinkler4@gmail.com>
2023-12-10 23:46:23 -0800

    solution to can place flowers

--- Arrays/Easy/CanPlaceFlowers.cpp
+++ Arrays/Easy/CanPlaceFlowers.cpp
@@ -5,7 +5,25 @@ using namespace std;
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        
+        // if edge case where we have no flowers return true
+        if(n == 0) return true;
+
+        // insert zeros at beginning and end of flowerbed to avoid out of bounds
+        flowerbed.insert(flowerbed.begin(),0);
+        flowerbed.push_back(0);
+
+        // loop through flowerbed starting at 1 and go until the second to last element since we added zeros at front and back
+        for(int i=1;i<flowerbed.size()-1;i++){
+            // if there are no flowers behind, at or in front of the current index set value at i to 1 and subtract from n flowers
+            if(flowerbed[i-1] == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0){
+                flowerbed[i] = 1;
+                n--;
+            }
+            // if we have no more flowers return true
+            if(n == 0) return true;
+        }
+        // return false if we have flowers still
+        return false;
     }
 };
 
