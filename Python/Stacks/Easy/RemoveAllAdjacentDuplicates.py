bea75feb4a33f220902db56bc2ba2c3fcd4c9361
Brandon <bwinkler4@gmail.com>
2024-01-10 22:47:58 -0800

    solution to remove all duplicates in string

--- Python/Stacks/Easy/RemoveAllAdjacentDuplicates.py
+++ Python/Stacks/Easy/RemoveAllAdjacentDuplicates.py
@@ -1,6 +1,15 @@
 class Solution:
     def removeDuplicates(self, s: str) -> str:
+        if(len(s) == 1 or len(s) == 0): return s        # if we only have one character or no characters return the string
 
+        stk = []                                        # initialize the stack to track duplicates
+        stk.append(s[0])                                # push the first letter of the string to make sure stack isn't empty
+
+        for i in range(1,len(s)):                       # loop through string to look for duplicates
+            if(stk and s[i] == stk[-1]): stk.pop()      # if the current character is equal to the top of the stack then remove the top
+            else: stk.append(s[i])                      # otherwise append the current character
+
+        return "".join(stk)                             # return the modifed string
 
 
 # driver program
