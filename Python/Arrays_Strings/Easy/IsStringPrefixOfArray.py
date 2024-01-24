2af045e916070fa726f5291605a7dfc4f9382e80
Brandon <bwinkler4@gmail.com>
2024-01-23 23:31:31 -0800

    solution to is string prefix of array

--- Python/Arrays_Strings/Easy/IsStringPrefixOfArray.py
+++ Python/Arrays_Strings/Easy/IsStringPrefixOfArray.py
@@ -3,7 +3,13 @@ from typing import List
 
 class Solution:
     def isPrefixString(self, s: str, words: List[str]) -> bool:
+        string = ""                         # create string variable to check against s
 
+        for i in words:                     # loop through words and add each string to variable
+            string += i
+            if(string == s): return True    # if we have our prefix before we get to the end return true
+        
+        return False                        # otherwise return false
 
 # driver program
 if __name__ == '__main__':
@@ -12,6 +18,8 @@ if __name__ == '__main__':
     a1 = ["i","love","leetcode","apples"]
     s2 = "iloveleetcode"
     a2 = ["apples","i","love","leetcode"]
+    s3 = "a"
+    a3 = ["aa","aaaa","banana"]
 
     # solution class instance
     Solution = Solution()
@@ -22,3 +30,6 @@ if __name__ == '__main__':
 
     if(Solution.isPrefixString(s2,a2)): print(f"{s2} is a prefix of {a2}")
     else: print(f"{s2} is not a prefix of {a2}")
+
+    if(Solution.isPrefixString(s3,a3)): print(f"{s3} is a prefix of {a3}")
+    else: print(f"{s3} is not a prefix of {a3}")
