3efc09416f003fc242b5920b994a4914b7a35857
Brandon <bwinkler4@gmail.com>
2024-01-24 20:44:53 -0800

    solution to smallest palindrome

--- Python/Arrays_Strings/Easy/SmallestPalindrome.py
+++ Python/Arrays_Strings/Easy/SmallestPalindrome.py
@@ -1,6 +1,18 @@
 class Solution:
     def makeSmallestPalindrome(self, s: str) -> str:
+        l = 0                               # make left pointer               
+        r = len(s)-1                        # make right pointer
+        s_list = list(s)                    # turn string into list to make mutable
 
+        while(l < r):                       # loop until left is greater than or equal to right
+            if(s_list[l] > s_list[r]):      # if left char is greater change it to right char
+                s_list[l] = s_list[r]
+            elif(s_list[l] < s_list[r]):    # if left char is less change right char to left char
+                s_list[r] = s_list[l]
+            l+=1                            # increment left
+            r-=1                            # decrement right
+
+        return "".join(s_list)
 
 # driver program
 if __name__ == '__main__':
