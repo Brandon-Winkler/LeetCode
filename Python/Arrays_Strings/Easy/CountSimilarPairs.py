ae4810c03112c3b57fc7ad22827220e1a667442d
Brandon <bwinkler4@gmail.com>
2024-02-12 15:58:43 -0800

    solution to counting pairs of similar strings

--- Python/Arrays_Strings/Easy/CountSimilarPairs.py
+++ Python/Arrays_Strings/Easy/CountSimilarPairs.py
@@ -1,9 +1,21 @@
 from typing import List
+from math import comb
 
 
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
+        table = {}                      # create a hash table to track occurences of similar strings       
+        ans = 0                         # initialize answer output
 
+        for w in words:                 # loop through words array
+            word = frozenset(w)         # convert each word into a set to reference for duplicates
+            if(word not in table):      # if we don't have a duplicate put the set into the table
+                table[word] = 1         
+            else:                       # otherwise add 1 to the frequency of the set
+                table[word] += 1
+        # use combinatorics to count unique duplicates and sum them into answer
+        ans = sum(comb(val,2) for val in table.values()) 
+        return ans
 
 # driver program
 if __name__ == '__main__':
