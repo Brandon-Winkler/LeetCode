83760d34d5f91b7b2d5ee3036f8edcac60585f66
Brandon <bwinkler4@gmail.com>
2024-02-22 20:24:17 -0800

    solution to finding all paths in a graph

--- Python/Trees_Graphs/Medium/AllPaths.py
+++ Python/Trees_Graphs/Medium/AllPaths.py
@@ -3,7 +3,21 @@ from typing import List
 
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-
+        # set answer array for output
+        ans = []
+        # initialize stack and push first node to traverse graph
+        stk = []
+        stk.append((0,[0]))
+        # begin dfs
+        while(len(stk) > 0):
+            # get current node and current path
+            node,path = stk.pop()
+            # if we reached the end of path then append path to answer output
+            if(node == len(graph)-1): ans.append(path)
+            # if the current node has neighbors then push neighbors and corresponding paths for them onto stack
+            stk.extend((n,path + [n]) for n in graph[node])
+        
+        return ans
 
 # driver program
 if __name__ == '__main__':
@@ -15,5 +29,5 @@ if __name__ == '__main__':
     Solution = Solution()
 
     # output
-    print(f"{Solution.allPathsSourceTarget(g1)} are all the paths from 0 to {len(g1)-1}")
-    print(f"{Solution.allPathsSourceTarget(g2)} are all the paths from 0 to {len(g2)-1}")
+    print(f"{Solution.allPathsSourceTarget(g1)} are all the paths from nodes 0 to {len(g1)-1}.")
+    print(f"{Solution.allPathsSourceTarget(g2)} are all the paths from nodes 0 to {len(g2)-1}.")
